961d18774f0923a3083c917bb057122347a37ab8
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2025-07-22 13:20:57 +0500

    Upd: fixed .env

--- my-music/src/components/MusicPage.tsx
+++ my-music/src/components/MusicPage.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { MusicCarousel } from "./MusicCarousel";
-import { MusicPlayer } from "./MusicPlayer";
 import '../index.css';
 
+const COVER_URL = process.env.REACT_APP_COVER_URL;
+const AUDIO_URL = process.env.REACT_APP_AUDIO_URL;
 
-const sampleTracks = [
-  { id: "1", title: "Family and Friends", artist: "Oklou", 
-    cover: "http://localhost:3002/cover/cover1.jpg", 
-    src: "http://localhost:3002/audio/track1.m4a" },
-  { id: "2", title: "Harvest Sky", artist: "Oklou", 
-    cover: "http://localhost:3002/cover/cover2.jpg", 
-    src: "http://localhost:3002/audio/track2.m4a" },
-  { id: "3", title: "Blade Bird", artist: "Oklou", 
-    cover: "http://localhost:3002/cover/cover3.png", 
-    src: "http://localhost:3002/audio/track3.m4a" },
+
+export const sampleTracks = [
+  {
+    id: "1",
+    title: "Family and Friends",
+    artist: "Oklou",
+    cover: `${COVER_URL}cover1.jpg`,
+    src: `${AUDIO_URL}track1.m4a`,
+  },
+  {
+    id: "2",
+    title: "Harvest Sky",
+    artist: "Oklou",
+    cover: `${COVER_URL}cover2.jpg`,
+    src: `${AUDIO_URL}track2.m4a`,
+  },
+  {
+    id: "3",
+    title: "Blade Bird",
+    artist: "Oklou",
+    cover: `${COVER_URL}cover3.png`,
+    src: `${AUDIO_URL}track3.m4a`,
+  },
 ];
+
 
 
 
